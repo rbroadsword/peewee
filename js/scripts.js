1086001b6256d90b3116dc5c656914a76ec5e6db
Ryan Broadsword <rbroadsword@gmail.com>
2022-04-21 19:25:15 -0700

    passed test for range function, returning a range of numbers

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//Business Logic 
+
+function range(number) {
+  let userNumber = parseInt(number); 
+  const range = [];
+  for (let i = 0; i <= userNumber; i++) {
+    range.push(i); 
+  }
+  return range; 
+}
